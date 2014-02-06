03708bcb8321dafb16f882e8dd5651fa9a887708
Thanakorn <oak_thanakorn@hotmail.co.nz>
2014-02-06 02:30:10 +0000

    Other files which were written earlier.

---
+++ temporaryFormants.m
@@ -0,0 +1,48 @@
+addpath('/Users/Thanakorn/Downloads/voicebox');
+childFilePath = '/Users/Thanakorn/Documents/3rd project/speech-data/childes - children/';
+adultFilePath = '/Users/Thanakorn/Documents/3rd project/speech-data/podcasts - adults/';
+
+childFile = dir(strcat(childFilePath, '*.wav'));
+adultFile = dir(strcat(adultFilePath, '*.wav'));
+childFormants = zeros(size(childFile, 1), 3);
+adultFormants = zeros(size(adultFile, 1), 3);
+i=1;
+
+% In determining formants, poles are ignored if any of the following hold:
+%        (a) they are on the real axis
+%        (b) they have bandwidth > t*frequency (if t>0)
+%        (c) they have bandwidth > -t (if t<=0)
+t = 100;
+
+
+%For each child wav file
+for file = childFile'
+    disp(i);
+    [data, FS] = readwav(strcat(childFilePath, file.name));
+    data = data(:, 1);
+    ar_coeffs = arburg(data,12);
+    [N,F,A,B] = lpcar2fm(ar_coeffs, 300);
+    childFormants(i, 1) = F(1);
+    childFormants(i, 2) = F(2);
+    childFormants(i, 3) = F(3);
+    i = i+1;
+end
+
+i = 1;
+%For each adult wav file
+for file = adultFile'
+    disp(i);
+    [data, FS] = readwav(strcat(adultFilePath, file.name));
+    data = data(:, 1);
+    ar_coeffs = arburg(data,12);
+    [N,F,A,B] = lpcar2fm(ar_coeffs, 200);
+    adultFormants(i, 1) = F(1);
+    adultFormants(i, 2) = F(2);
+    adultFormants(i, 3) = F(3);
+    i = i+1;
+end
+
+scatter3(adultFormants(:, 1), adultFormants(:, 2), adultFormants(:, 3), 10, 'r');
+hold on;
+scatter3(childFormants(:, 1), childFormants(:, 2), childFormants(:, 3), 10, 'g');
+hold off;
---
+++ temporaryPitch.m
@@ -0,0 +1,35 @@
+addpath('/Users/Thanakorn/Downloads/voicebox');
+childFilePath = '/Users/Thanakorn/Documents/3rd project/speech-data/childes - children/';
+adultFilePath = '/Users/Thanakorn/Documents/3rd project/speech-data/podcasts - adults/';
+
+childFile = dir(strcat(childFilePath, '*.wav'));
+adultFile = dir(strcat(adultFilePath, '*.wav'));
+childPitch = zeros(size(childFile, 1), 1);
+adultPitch = zeros(size(adultFile, 1), 1);
+i = 1;
+
+%For each child wav file
+for file = childFile'
+    [data, FS] = readwav(strcat(childFilePath, file.name));
+    data = data(:, 1);
+    pitch = fxpefac(data, FS);
+    pitch = mean(pitch);
+    childPitch(i, 1) = pitch;
+    i = i+1
+end
+
+i = 1
+%For each adult wav file
+for file = adultFile'
+    [data, FS] = readwav(strcat(adultFilePath, file.name));
+    data = data(:, 1);
+    pitch = fxpefac(data, FS);
+    pitch = mean(pitch);
+    adultPitch(i, 1) = pitch;
+    i = i+1
+end
+
+ scatter(adultPitch, (1:size(adultPitch, 1)), 10, 'g');
+ hold on;
+ scatter(childPitch, (1:size(childPitch, 1)), 10, 'r');
+ hold off;
